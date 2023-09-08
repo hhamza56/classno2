221899ffae4b9b8606f8931331bc11eca814931a
Muhammad Hamza <143834218+hhamza56@users.noreply.github.com>
2023-09-08 20:09:39 +0100

    wrok complete

---
+++ apps.js
@@ -0,0 +1,23 @@
+alert("hamza")
+alert("hussain")
+alert("hazmahusaint657:gmail.com")
+alert("phone number")
+alert("passward")
+alert("Correct this statement: alert Youre learning JavaScript!")
+alert("hello sir hum ye seakhna chaty hain ap mharbani karay hamy acha pharayn")
+
+
+var name= "myVariableName"
+console.log(name)
+
+var myVariable ="hello world"
+console.log(myVariable)
+
+alert("my team and my friend")
+
+
+
+
+
+
+
